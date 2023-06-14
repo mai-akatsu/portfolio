938f2e70019cd302a9a0e3d07e6ff2cb0e259bb4
mai-akatsu <debunyankawaii@gmail.com>
2023-06-14 20:58:31 +0900

    編集機能と削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,22 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
+  end
 end
