3116065cc47cab1c451bdc5c7bea285dbf6aee37
mai-akatsu <debunyankawaii@gmail.com>
2023-06-16 01:01:29 +0900

    アバター画像の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "9.jpeg"
+    )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
@@ -29,6 +33,12 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpeg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+    
     if @user.save
       flash[:notice] = "ユーザー情報を更新しました"
       redirect_to("/users/#{@user.id}")
